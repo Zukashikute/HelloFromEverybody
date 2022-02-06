013099d96a365b64e105f5462f1559fbec60c4dc
Kimball Newton <new17005@byui.edu>
2022-02-05 18:09:56 -0700

    git commit

--- src/balajadia/Main.java
+++ src/balajadia/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jared.");
+        System.out.println("Hello Jared, My name is Kimball.");
     }
 }
